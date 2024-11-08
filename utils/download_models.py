9763e8d295679d0b335f242f7a0e345e7db48e96
TuongHH <tuongho.dev@gmail.com>
2024-11-08 17:30:15 +0700

    [feat]: download model

--- utils/download_models.py
+++ utils/download_models.py
@@ -1,7 +1,10 @@
 import gdown
 import os
 
-url = 'https://drive.usercontent.google.com/download?id=1_iyLz-nl5ezvspezRfEO16v820IZ0aeS'
+# url = 'https://drive.google.com/uc?id=157gqq8aoNmrj9B35-sxeaFStsmDe891H'
+# output = './assets/det_model.zip'
+
+url = 'https://drive.google.com/uc?id=1Ok3gNQa_7RcBBvUakDIYMdbaW6POMagK'
 output = './assets/det_model.pt'
 
 # check if the file is already downloaded
